05dff676614c3614c491d2fb708d747595f82b30
Pavithra535 <pavithramk535@gmail.com>
2025-08-12 15:46:49 +0530

    Week3

---
+++ Week3-SpringCore-Maven-SpringDataJPA/Ex2-DependencyInjection/LibraryManagementApplication.java
@@ -0,0 +1,20 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve the bean
+        BookService bookService = context.getBean("bookService", BookService.class);
+
+        // Test the DI
+        bookService.displayBook();
+
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
